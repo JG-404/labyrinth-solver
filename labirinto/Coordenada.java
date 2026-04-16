6ad9b5bdb681a650595f604c14c35e5e2e89d02a
cc25330 <jotagemurerleite@gmail.com>
2026-04-16 18:08:31 -0300

    Construtor de ResolvedorLabirinto criado

--- labirinto/Coordenada.java
+++ labirinto/Coordenada.java
@@ -1,5 +1,4 @@
 package labirinto;
-
 public class Coordenada {
     private int x;
     private int y;
@@ -12,13 +11,21 @@ public class Coordenada {
     public int getX() {
         return x;
     }
+
     public void setX(int x) {
         this.x = x;
     }
+
     public int getY() {
         return y;
     }
+
     public void setY(int y) {
         this.y = y;
+    }
+
+    @Override
+    public String toString(){
+        return "(" + this.x + "," + this.y + ")";
     }
 }
--- labirinto/ResolvedorLabirinto.java
+++ labirinto/ResolvedorLabirinto.java
@@ -1,10 +1,44 @@
 package labirinto;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import estruturas.Fila;
 import estruturas.Pilha;
 
 public class ResolvedorLabirinto{
-    
+
     private Pilha<Coordenada> caminho;
     private Pilha<Fila<Coordenada>> possibilidades;
+    private Fila<Coordenada> fila;
+
+    private char[][] labirinto;
+
+    private Coordenada atual;
+
+    public ResolvedorLabirinto(String path){
+        try{
+            List<String> arquivo = Files.readAllLines(Paths.get("./teste1.txt"), StandardCharsets.UTF_8);
+                
+            int x = Integer.parseInt(arquivo.get(0));
+            int y = Integer.parseInt(arquivo.get(1));
+            
+            this.labirinto = new char[x][y];
+
+            for(int i = 0; i < x; i++){
+                for (int j = 0; j < y; j++){
+                    this.labirinto[i][j] = arquivo.get(i + 2).charAt(j);
+                }
+            }
+
+            this.caminho = new Pilha<>(x * y);
+            this.possibilidades = new Pilha<>(x * y);
+            this.fila = new Fila<>(3);
+        }
+        catch(Exception error){
+            System.err.println(error.getMessage());
+        }
+    }
 }
